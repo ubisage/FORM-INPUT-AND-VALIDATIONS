414e7f8b695d89e09c4246a9793dd9c4ce0037bc
ubisage <obaiduzair822@gmail.com>
2023-01-23 11:10:48 +0530

    Solved prev issue added another state to see if it was used or for submitted which means already used

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,16 +1,25 @@
-import { useState} from 'react'
+import { useState,useEffect} from 'react'
 
 const SimpleInput = (props) => {
   // const nameInputRef = useRef()
 
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false) 
+   useEffect (() => {
+        if (enteredNameIsValid){
+        console.log('naame is valid')
+      }
+    }, [enteredNameIsValid])
+   
 
   const nameChangeHandler = (event)=>{
     setEnteredName(event.target.value);
   }
   const formSubmissionHandler = (event)=>{
     event.preventDefault();
+
+    setEnteredNameTouched(true)
 
     if(enteredName.trim() ===''){
       setEnteredNameIsValid(false)
@@ -25,15 +34,16 @@ const SimpleInput = (props) => {
     // console.log(enteredValue);
     // nameInputRef.current.value=""; 
   }
+  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+  const nameInputClasses = nameInputIsInValid ? 'form-control invalid' : 'form-control'
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         {/* <input ref={nameInputRef} type='text' id='name' onChange={nameChangeHandler} /> */}
         <input  type='text' id='name' value={enteredName} onChange={nameChangeHandler} />
-        {!enteredNameIsValid && <p className='error-text'>Name must not be empty</p>}
+        {nameInputIsInValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
