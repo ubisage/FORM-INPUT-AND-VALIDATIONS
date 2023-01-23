5202ded6d6bc8b95e9f1b86897c8f3c26c15d756
ubisage <obaiduzair822@gmail.com>
2023-01-23 10:58:05 +0530

    have an issue setEnteredNameisVAid to true to properly display error msg correctly but if useeffect was there is a issue cause inthe beg its not actually true

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,8 +1,10 @@
-import {useRef, useState} from 'react'
-const SimpleInput = (props) => {
-  const nameInputRef = useRef()
+import { useState} from 'react'
 
-  const [enteredName, setEnteredName] = useState('')
+const SimpleInput = (props) => {
+  // const nameInputRef = useRef()
+
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
 
   const nameChangeHandler = (event)=>{
     setEnteredName(event.target.value);
@@ -10,15 +12,28 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event)=>{
     event.preventDefault();
 
+    if(enteredName.trim() ===''){
+      setEnteredNameIsValid(false)
+      return;
+    }
+
+    setEnteredNameIsValid(true)
+
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value
-    console.log(enteredValue);
+    setEnteredName('')
+    // const enteredValue = nameInputRef.current.value
+    // console.log(enteredValue);
+    // nameInputRef.current.value=""; 
   }
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameChangeHandler} />
+        {/* <input ref={nameInputRef} type='text' id='name' onChange={nameChangeHandler} /> */}
+        <input  type='text' id='name' value={enteredName} onChange={nameChangeHandler} />
+        {!enteredNameIsValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
