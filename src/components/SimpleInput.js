db315cd3c8fe7dec6ff0b11a47463adf5e4bb1c8
ubisage <obaiduzair822@gmail.com>
2023-01-23 11:55:43 +0530

    Onblur -form valid when focus lost

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,49 +1,69 @@
-import { useState,useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   // const nameInputRef = useRef()
 
-  const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false) 
-   useEffect (() => {
-        if (enteredNameIsValid){
-        console.log('naame is valid')
-      }
-    }, [enteredNameIsValid])
-   
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("naame is valid");
+    }
+  }, [enteredNameIsValid]);
 
-  const nameChangeHandler = (event)=>{
+  const nameChangeHandler = (event) => {
     setEnteredName(event.target.value);
-  }
-  const formSubmissionHandler = (event)=>{
-    event.preventDefault();
+  };
 
+  const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true)
-
-    if(enteredName.trim() ===''){
-      setEnteredNameIsValid(false)
+    
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
       return;
     }
 
-    setEnteredNameIsValid(true)
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
 
     console.log(enteredName);
-    setEnteredName('')
+    setEnteredName("");
     // const enteredValue = nameInputRef.current.value
     // console.log(enteredValue);
-    // nameInputRef.current.value=""; 
-  }
-  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched
+    // nameInputRef.current.value="";
+  };
+  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched;
 
-  const nameInputClasses = nameInputIsInValid ? 'form-control invalid' : 'form-control'
+  const nameInputClasses = nameInputIsInValid
+    ? "form-control invalid"
+    : "form-control";
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
-        <label htmlFor='name'>Your Name</label>
+        <label htmlFor="name">Your Name</label>
         {/* <input ref={nameInputRef} type='text' id='name' onChange={nameChangeHandler} /> */}
-        <input  type='text' id='name' value={enteredName} onChange={nameChangeHandler} />
-        {nameInputIsInValid && <p className='error-text'>Name must not be empty</p>}
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameChangeHandler}
+          onBlur={nameInputBlurHandler}
+        />
+        {nameInputIsInValid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
